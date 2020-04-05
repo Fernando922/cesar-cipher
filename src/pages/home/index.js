f4798c34f8f1ed7151ffde15818a5563d147da64
Fernando de Paula <di.paulaf@hotmail.com>
2020-04-05 02:44:53 -0300

    finished

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { alphabet, returnCrypto } from "../../utils";
-
 import api, { token } from "../../services/api";
+import "./styles.css";
 
 export default function Home() {
   const [obj, setObj] = useState({});
+  const [responseObject, setResponseObject] = useState({});
+  const [file, setFile] = useState();
 
   useEffect(() => {
     function fetchData() {
       return api.get(`/generate-data?token=${token}`);
     }
 
-    fetchData().then(res => setObj(res.data));
+    fetchData().then((res) => setObj(res.data));
   }, []);
 
   useEffect(() => {
@@ -21,7 +23,7 @@ export default function Home() {
       const messageArray = secretMessage.split("");
 
       function replaceChar(char, index, array) {
-        const position = alphabet.findIndex(element => element === char);
+        const position = alphabet.findIndex((element) => element === char);
         if (position >= 0) {
           array[index] = alphabet[position + step];
         } else {
@@ -33,40 +35,63 @@ export default function Home() {
         replaceChar(char.toLocaleLowerCase(), index, array)
       );
 
-
       let newObj = {
         ...obj,
         decifrado: messageArray.join(""),
-        resumo_criptografico: returnCrypto(obj.decifrado)
+        resumo_criptografico: returnCrypto(obj.decifrado),
+      };
+
+      setResponseObject(newObj);
+
+      download(JSON.stringify(newObj), "answer.json", "text/plain");
+
+      function download(content, fileName, contentType) {
+        var a = document.createElement("a");
+        var file = new Blob([content], { type: contentType });
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.text = "Baixar arquivo JSON";
+        document.getElementsByClassName("container")[0].appendChild(a);
       }
-
-      setObj(newObj)
-      
-
-      // fs.writeFile(`${__dirname}/answer.json`, JSON.stringify(obj), err => {
-      //   console.log(err || 'Arquivo salvo!');
-      // });
     }
   }, [obj]);
 
-  function sendFile(e) {
+  function onSelectArchive(e) {
+    setFile(e.target.files[0]);
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
-    alert("enviado!!");
+    if (!file) {
+      alert("selecione um arquivo para enviar!");
+    }
+    let formData = new FormData();
+    formData.append("answer", file);
+
+    api
+      .post(`/submit-solution?token=${token}`, formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then(alert("JSON enviado com sucesso!"))
+      .catch((e) => console.log(e));
   }
 
   return (
-    <form method="post" onSubmit={sendFile}>
-      <div>
-        <p>Numero de casas para criptografar: {obj.numero_casas}</p>
-        <p>Cifra: {obj.cifrado}</p>
-        <p>Decifrado: {obj.decifrado}</p>
-        <p>Sha1: {obj.resumo_criptografico}</p>
-        <label for="file">Escolha o arquivo para enviar</label>
-        <input type="file" id="1" name="answer" accept=".json" />
-      </div>
-      <div>
-        <button>Submit</button>
-      </div>
-    </form>
+    <div className="container">
+      <h1>Cifra de César</h1>
+      <form action="post" onSubmit={handleSubmit}>
+        <p>Numero de casas para criptografar: {responseObject.numero_casas}</p>
+        <p>Cifra: {responseObject.cifrado}</p>
+        <p>Decifrado: {responseObject.decifrado}</p>
+        <p>Sha1: {responseObject.resumo_criptografico}</p>
+        <label>Escolha o arquivo para enviar</label>
+        <input type="file" accept=".json" onChange={onSelectArchive} />
+        <div>
+          <button>Enviar!</button>
+        </div>
+      </form>
+    </div>
   );
 }
